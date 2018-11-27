ff303ef2d6ec762a094e7e912462530f2df568fc
Kevin Hallett <kevinmhallett@gmail.com>
2018-11-27 15:29:49 -0600

    add opportunities link

--- src/app/features/advisor/header/header.component.ts
+++ src/app/features/advisor/header/header.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeaderLink } from '../../../interfaces/header-link';
+
 @Component({
   selector: 'advisor-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  public links: HeaderLink[] = [
+    {
+      href: '/advisor',
+      text: 'Dashboard'
+    },
+    {
+      href: './contacts',
+      text: 'Contacts'
+    },
+    {
+      href: './opportunities',
+      text: 'Opportunities'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
